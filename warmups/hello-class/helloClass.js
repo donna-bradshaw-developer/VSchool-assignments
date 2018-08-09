b9659fbee6ebb54ff0627fdd02b076ff6fd569e6
Donna <donna.bradshaw.developer@gmail.com>
2018-08-09 08:57:00 -0400

    warmup day 1

---
+++ warmups/hello-class/helloClass.js
@@ -0,0 +1,9 @@
+//Define a function that prints "Hello, Class" to the terminal window.
+
+//Call/Invoke that function to make it work.
+
+function hello (){
+    return "Hello, Class!"
+}
+
+console.log (hello());
