ccfd581d0c233119e7c61cf7e7b282e96f8578e8
Donna <donna.bradshaw.developer@gmail.com>
2018-08-21 13:35:17 -0400

    day 3

---
+++ exercises/even-odd-looping/README.md
@@ -0,0 +1,3 @@
+Even / Odd Looping
+--------------------------------
+Simple looping exercise that return even or odd for numbers 0 - 100
---
+++ exercises/even-odd-looping/evenOddLooping.js
@@ -0,0 +1,11 @@
+const loop = () => {
+    for (i = 0; i < 101; i++){
+        if (i % 2 === 0){
+            console.log (`${i} is even`)
+        }else{
+            console.log(` ${i} is odd`)
+        }
+    }
+}
+
+loop();
